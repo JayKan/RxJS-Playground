c229c557c7301a11fbcd515f7ff8b9b96780f5c5
Jay Kan <jay.kan@matrixcos.com>
2015-11-13 15:01:24 -0800

    Inital commit with RxJS letter counter example

---
+++ lettercount/custom.js
@@ -0,0 +1,28 @@
+;(function(){
+
+  'use strict';
+
+  var countElement   = document.getElementById('toCount');
+  var resultElement  = document.getElementById('result');
+  var result2        = document.getElementById('result2');
+
+  var source = Rx.Observable.fromEvent(countElement, 'keyup')
+    .map(function(e){
+      return 'length: ' + e.target.value.length;
+    })
+    .distinctUntilChanged();
+
+  function setHtml(text){
+    this.innerHTML = text;
+  }
+
+
+  //source.subscribe( setHtml.bind(resultElement) );
+
+  source.subscribe(function(text){
+    resultElement.innerHTML  = text;
+  });
+
+  source.subscribe( setHtml.bind( result2 ));
+
+})();
