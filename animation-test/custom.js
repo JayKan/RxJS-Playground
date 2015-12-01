08b60997817ea36f64e6b5ca99cc0898d84ac15e
Jay Kan <leicasper@gmail.com>
2015-12-01 15:09:16 -0800

    Add RxJS anmidation demo

--- animation-test/custom.js
+++ animation-test/custom.js
@@ -4,6 +4,34 @@
 
   console.log('Animation Test loading');
 
+  var fromEvent = Rx.Observable.fromEvent;
+
+  /**
+   * Extends RxJS Observable
+   * @method movingWindow
+   * @param size {number}
+   * @param selector {Function} Current box
+   * @param onShift {Function}
+   */
+  Rx.Observable.prototype.movingWindow = function(size, selector, onShift) {
+    var fn = this;
+    return Rx.Observable.create(function(observable) {
+      var arr = [];
+      return fn.subscribe(
+        function(x) {
+          var item = selector(x);
+          arr.push(item);
+          if (arr.length > size) {
+            var i = arr.shift();
+            onShift(i);
+          }
+        },
+        function(e) { observable.onError(e); },
+        function() { observable.onCompleted(); }
+      );
+    });
+  };
+
   /**
    * @class
    * @constructor
@@ -23,7 +51,7 @@
   Box.prototype = {
     /**
      * @method buildBox
-     * @returns {*}
+     * @returns Box instance for chaining.
      */
     buildBox: function buildBox(){
       var box = $("<div class=\"box\" id=\"" + this.id + "\">").css({
@@ -38,12 +66,17 @@
       this.parent.append(box);
       return this;
     },
+
     /**
-     * @method showBox
+     * Finds the parent <div> with current box.id and then
+     * adds jQuery fadeIn animation.
      */
     showBox: function(){
       return this.parent.find('#' + this.id).fadeIn('fast');
     },
+    /**
+     * Find the parent <div> and then remove current box.
+     */
     hideBox: function(){
       return this.parent.find("#" + this.id).fadeOut('fast', function() {
         return $(this).remove();
@@ -62,7 +95,20 @@
     return Math.floor(Math.random() * (high + 1)) - low;
   }
 
-
-
-
+  function load(){
+    var canvasElement = $('#drawing');
+    return Rx.Observable.fromEvent(canvasElement, 'mousemove')
+      .movingWindow(
+        25,
+        function(x) {
+          var box = new Box([x.clientX, x.clientY], canvasElement);
+          box.showBox();
+          return box;
+        },
+        function(box){
+          box.hideBox();
+        }
+      ).subscribe();
+  }
+  window.onload = load;
 })();
