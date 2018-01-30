67828dfe2c979d5b7b41a8ba701e6df8cd8405e2
Jay Kan <leicasper@gmail.com>
2018-01-30 01:08:51 -0800

    refactor(examples): clean up Mario example

--- mario/custom.js
+++ mario/custom.js
@@ -1,29 +1,26 @@
-;(function(){
-
+;(function () {
   'use strict';
-
+  
   var slice = Array.prototype.slice,
       Observable = Rx.Observable;
 
   var dom = {};
-  ['resize', 'keyup', 'keydown'].forEach(function(event) {
-    dom[event] = function(element) {
+  ['resize','keyup','keydown'].forEach(function (event) {
+    dom[event] = function (element) {
       return Observable.fromEvent(element, event);
     };
   });
 
-  console.log('Dom: ', dom);
-
-  // Compose functions
+  // compose functions
   function compose() {
     return slice.call(arguments, 0).reduce(function(f, g) {
-      return function(){
+      return function () {
         return f(g.apply(null, arguments));
       };
     });
   }
 
-  // Update record
+  // update record
   function update(r, u) {
     return typeof u === 'function' ?
       update(r, u(r)) :
@@ -31,22 +28,25 @@
   }
 
   // Environment functions
+
   function jump(dt) {
-    return function(m) {
+    return function (m) {
       return dt.y > 0 && m.y === 0 ?
-        update(m, {vy: 5}) : m;
+        update(m, {vy: 5}) :
+        m;
     };
   }
 
   function gravity(t) {
-    return function(m) {
+    return function (m) {
       return m.y > 0 ?
-        update(m, {vy: m.vy - t/ 4}): m;
+        update(m, {vy: m.vy - t / 4}) :
+        m;
     };
   }
 
   function physics(t) {
-    return function(m) {
+    return function (m) {
       return update(m, {
         x: m.x + t * m.vx,
         y: Math.max(0, m.y + t * m.vy)
@@ -55,13 +55,14 @@
   }
 
   function walk(dt) {
-    return function(m){
+    return function (m) {
       var dir = m.dir;
       if (dt.x < 0) {
         dir = 'left';
       } else if (dt.x > 0) {
         dir = 'right';
       }
+
       return update(m, {
         vx: dt.x,
         dir: dir
@@ -73,24 +74,19 @@
     return compose(jump(keys), gravity(dt), walk(keys), physics(dt));
   }
 
-  /**
-   * @method render
-   * @param dimensions
-   * @param mario
-   * @param marioImage
-   */
+  // Render
   function render(dimensions, mario, marioImage) {
     var verb = 'stand';
     if (mario.y > 0) {
       verb = 'jump';
-    } else if (mario.vx !== 0 ) {
+    } else if (mario.vx !== 0) {
       verb = 'walk';
     }
 
     var src = 'img/' + verb + '-' + mario.dir + '.gif';
 
     // gif animations reset on src assignment
-    if (marioImage.name !== src) {
+    if(marioImage.name !== src) {
       marioImage.src = src;
       marioImage.name = src;
     }
@@ -99,30 +95,32 @@
     marioImage.style.top = (dimensions.height - 91 - mario.y) + 'px';
   }
 
-  // Element's FPS
+  // Elm's FPS
   function fps(v) {
     return Observable.interval(1000 / v)
       .timestamp()
       .bufferWithCount(2, 1)
-      .map(function(w) { return w[1].timestamp - w[0].timestamp; })
+      .map(function (w) { return w[1].timestamp - w[0].timestamp; })
       .share();
   }
 
   function keysBuffer(buffer, e) {
     var result = buffer.slice(0);
     if (e.type === 'keydown') {
-      if (buffer.indexOf(e.keyCode) !== -1) {
+      if(buffer.indexOf(e.keyCode) === -1) {
         result.push(e.keyCode);
       }
     } else {
-      result = buffer.filter(function(keyCode){ return keyCode !== e.keyCode });
+      result = buffer.filter(function (keyCode) {return keyCode !== e.keyCode; });
     }
+
     return result;
   }
 
   // Set up the environment
+
   var dimensions = dom.resize(window)
-    .map(function(e) {
+    .map(function (e) {
       return {
         width: e.target.innerWidth,
         height: e.target.innerHeight
@@ -134,30 +132,33 @@
     });
 
   var keyDowns = dom.keydown(document);
-  var keyUps = dom.keyup(document);
+  var keyUps   = dom.keyup(document);
 
-  // Array of currently pressed keys
+  // array of currently pressed keys
   var keyboard = keyDowns
     .merge(keyUps)
     .scan(keysBuffer, [])
     .distinctUntilChanged()
     .shareValue([]);
 
-  // LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40
+  // LEFT: 37
+  // UP: 38
+  // RIGHT: 39
+  // UP: 40
   var arrowsMap = {
     37: {x: -1, y: 0},
-    38: {x: 0,  y: 1},
-    39: {x: 1,  y: 0},
-    40: {x: 0,  y: -1}
+    39: {x: 1, y: 0},
+    38: {x: 0, y: 1},
+    40: {x: 0, y: -1}
   };
 
-  // Element's Keyboard.arrows
+  // Elm's Keyboard.arrows
   var arrows = keyboard
-    .map(function(keys) {
+    .map(function (keys) {
       return keys
-        .filter(function(key) { return key in arrowsMap; })
-        .reduce(function(agg, key) {
-          return update(agg, function(prev) {
+        .filter(function (key) { return key in arrowsMap; })
+        .reduce(function (agg, key) {
+          return update(agg, function (prev) {
             return {
               x: prev.x + arrowsMap[key].x,
               y: prev.y + arrowsMap[key].y
@@ -166,32 +167,32 @@
         }, {x: 0, y: 0});
     });
 
-  var deltas = fps(60).map(function(t) { return t / 20; });
+  var deltas = fps(60).map(function (t) { return t / 20; });
   var input = deltas
-    .combineLatest(arrows, function(dt, keys) { return { dt: dt, keys: keys}; })
+    .combineLatest(arrows, function (dt, keys) { return { dt: dt, keys: keys }; })
     .sample(deltas);
 
   var marioImage = document.getElementById('mario');
 
-  // Mario
+  // mario
   var mario = {
-    x   : 0,
-    y   : 0,
-    vx  : 0,
-    vy  : 0,
-    dir : 'right'
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    dir: 'right'
   };
 
-  var marios = input.scan(function(m, c) {
+  var marios = input.scan(function (m, c) {
     return step(c.dt, c.keys)(m);
   }, mario);
 
   marios
-    .combineLatest(dimensions, function(mario, dimensions) {
+    .combineLatest(dimensions, function (mario, dimensions) {
       return { mario: mario, dimensions: dimensions };
     })
     .subscribe(
-      function(c) { render(c.dimensions, c.mario, marioImage); },
-      function(err) { console.log(err); }
+      function (c) { render(c.dimensions, c.mario, marioImage); },
+      function (err) { console.log(err); }
     );
-})();
+}());
--- mario/polyfills.js
+++ mario/polyfills.js
@@ -1,20 +1,22 @@
-;(function(){
+;(function () {
+  'use strict';
+  
   if (!Object.assign) {
-    Object.defineProperty(Object, 'assign', {
+    Object.defineProperty(Object, "assign", {
       enumerable: false,
       configurable: true,
       writable: true,
       value: function(target, firstSource) {
-        'use strict';
+        "use strict";
         if (target === undefined || target === null) {
-          throw new TypeError('Cannot convert first argument to Object.');
+          throw new TypeError("Cannot convert first argument to object");
         }
         var to = Object(target);
-        for (var i = 1, len = arguments.length; i < len; i+=1) {
+        for (var i = 1; i < arguments.length; i++) {
           var nextSource = arguments[i];
           if (nextSource === undefined || nextSource === null) continue;
           var keysArray = Object.keys(Object(nextSource));
-          for (var nextIndex = 0, ii = keysArray.length; nextIndex < ii; nextIndex+=1) {
+          for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
             var nextKey = keysArray[nextIndex];
             var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
             if (desc !== undefined && desc.enumerable) { to[nextKey] = nextSource[nextKey]; }
@@ -22,6 +24,6 @@
         }
         return to;
       }
-    })
+    });
   }
-})();
+}());
