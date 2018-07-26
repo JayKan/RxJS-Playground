8f2a5c65ccf9435c58ffd585d016648b7a07d8ad
Jay Kan <leicasper@gmail.com>
2018-07-25 20:06:27 -0700

    example(react): add WIP react-recompose-demo

---
+++ react-recompoes-demo/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          Hello World
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
