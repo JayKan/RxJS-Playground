1e8f0cc7ef8cad444a0346d73ab3bbf9c17762d1
Jay Kan <leicasper@gmail.com>
2018-07-25 20:14:35 -0700

    example(react): add react example on how to configure recompose with rxjs

--- react-recompoes-demo/src/App.js
+++ react-recompoes-demo/src/App.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
+// import React from 'react';
+// import { Observable } from 'rxjs';
+// import config from 'recompose/rxjsObservableConfig';
+// import {
+//   setObservableConfig,
+//   componentFromStream
+// } from 'recompose';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p className="App-intro">
-          Hello World
-        </p>
-      </div>
-    );
-  }
-}
+// setObservableConfig(config);
+
+// // class App extends Component {
+// //   render() {
+// //     return (
+// //       <div className="App">
+// //         <p className="App-intro">
+// //           Hello World
+// //         </p>
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// const App = componentFromStream(props$ => {
+//   return Observable.interval(1000).map(i => (
+//     <div>{i}</div>
+//   ));
+// });
+
+// export default App;
+import React from "react"
+import { render } from "react-dom"
+import { Observable } from "rxjs"
+import config from "recompose/rxjsObservableConfig"
+import {
+  setObservableConfig,
+  componentFromStream
+} from "recompose"
+
+setObservableConfig(config)
+
+const App = componentFromStream(props$ => {
+  return Observable.interval(1000).map(i => (
+    <div>{i}</div>
+  ))
+})
 
 export default App;
